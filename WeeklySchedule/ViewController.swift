d548abc63e4fde246571a202747b84ac8367eb4b
Yi <yi2009a@gmail.com>
2014-12-29 19:55:03 -0600

    Initial Commit

---
+++ WeeklySchedule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeeklySchedule
+//
+//  Created by Yi Chen on 12/29/14.
+//  Copyright (c) 2014 YC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
